be5c001f4723fedce30aad9953a8cb91c7e21ac8
elmardeveducation <elmarh@deveducation.com>
2020-08-20 13:30:35 +0400

    sdelan 1 primer

---
+++ Mygitproject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mygitproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Privet iz gita!");
+        }
+    }
+}
